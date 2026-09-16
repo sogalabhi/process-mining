f2f18b4b8139569bdcd71aa5d31f216ca8c4f503
sogalabhi <abhijithsogal@gmail.com>
2026-09-16 22:47:06 +0530

    init project

---
+++ src/main/java/com/abhi/processmining/ProcessMiningApplication.java
@@ -0,0 +1,13 @@
+package com.abhi.processmining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessMiningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessMiningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhi/processmining/ProcessMiningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.processmining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessMiningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
